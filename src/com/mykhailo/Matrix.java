01698f0642a888018fac233a52995a3986877d61
Mykhailo <m_musienko@outlook.com>
2022-02-02 20:19:50 +0200

    Update Matrix.java

--- src/com/mykhailo/Matrix.java
+++ src/com/mykhailo/Matrix.java
@@ -1,8 +1,28 @@
 package com.mykhailo;
 
+import java.util.Arrays;
+
 public class Matrix {
 
+    public static final int[][] MATRIX = {
+            {10, 11, 12, 13},
+            {14, 15, 16, 17},
+            {18, 19, 20, 21}
+    };
+
     public static void main(String[] args) {
+
+        print(MATRIX);
+        System.out.println();
+        print(swapColumns(MATRIX, 1, 2));
+        System.out.println();
+        print(swapRows(MATRIX, 1, 2));
+        System.out.println();
+        print(multiple(MATRIX, 2));
+        System.out.println();
+        System.out.println(Arrays.toString(toArray(MATRIX)));
+        System.out.println();
+        System.out.println(maxValue(MATRIX));
 
     }
 
@@ -16,4 +36,59 @@ public class Matrix {
     max(int[][] matrix) - возвращает максимальный элемент матрицы
     */
 
+    public static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] swapColumns(int[][] matrix, int firstColumn, int secondColumn) {
+        for (int i = 0; i < matrix.length; i++) {
+            int temp = matrix[i][firstColumn - 1];
+            matrix[i][firstColumn - 1] = matrix[i][secondColumn - 1];
+            matrix[i][secondColumn - 1] = temp;
+        }
+        return matrix;
+    }
+
+    public static int[][] swapRows(int[][] matrix, int firstRow, int secondRow) {
+        int[] tmpRow = matrix[firstRow - 1];
+        matrix[firstRow - 1] = matrix[secondRow - 1];
+        matrix[secondRow - 1] = tmpRow;
+        return matrix;
+    }
+
+    public static int[][] multiple(int[][] matrix, int value) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] *= value;
+            }
+        }
+        return matrix;
+    }
+
+    public static int[] toArray(int[][] matrix) {
+        int[] array = new int[matrix.length * matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                array[i * matrix[i].length + j] = matrix[i][j];
+            }
+        }
+        return array;
+    }
+
+    public static int maxValue(int[][] matrix) {
+        int maxValue = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (maxValue < matrix[i][j]) {
+                    maxValue = matrix[i][j];
+                }
+            }
+        }
+        return maxValue;
+    }
 }
