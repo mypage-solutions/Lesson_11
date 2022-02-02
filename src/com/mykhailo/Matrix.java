b2f71eb30469ba0a4dc64c7aeb6eb27408f5a1c2
Mykhailo <m_musienko@outlook.com>
2022-02-02 18:05:48 +0200

    Add Matrix.java

---
+++ src/com/mykhailo/Matrix.java
@@ -0,0 +1,19 @@
+package com.mykhailo;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    2*. Написать класс для работы с матрицами (2-мерными массивами), методы:
+    print(int[][] matrix) - печатает матрицу
+    swapColumns(int[][] matrix, i, j) - меняет 2 столбца местами
+    swapRows(int[][] matrix, i, j) - меняет 2 рядка местами
+    mul(int[][] matrix, int value) - умножает матрицу на число
+    toArray(int[][] matrix) - превращает матрицу в массив. Например если была матрица на 3x4 то вернуть массив из 12 элементов
+    max(int[][] matrix) - возвращает максимальный элемент матрицы
+    */
+
+}
